5258262624dba298d7f057948f6f6f00bb6d9f58
MDario123 <manuel.dario.oliver@gmail.com>
2024-12-13 05:13:06 +0100

    fix: incorrect routing

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route path="/:videoId" element={<VideoPlayer />} />
+          <Route path=":videoId" element={<VideoPlayer />} />
         </Route>
       </Routes>
     </BrowserRouter>
