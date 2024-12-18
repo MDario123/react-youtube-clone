0f7d8ab95edec7e2842179ef8eea68944f0c2a03
MDario123 <manuel.dario.oliver@gmail.com>
2024-12-18 16:12:45 +0100

    fix: use correct base in development and production

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { VideoPlayer } from "./components/VideoPlayer.tsx";
 
+const basePath =
+  import.meta.env.MODE !== "development" ? import.meta.env.VITE_BASE_PATH : "/";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basePath}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route path=":videoId" element={<VideoPlayer />} />
